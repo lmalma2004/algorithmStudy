54f092b644b3fdaf4f8a535b05d2e33c9b0b27a4
lmalma2004 <lmalma2004@naver.com>
2020-04-09 18:11:21 +0900

    백준/2042 - 세그먼트트리 - 갱신연산적용 - 통과

--- 구간합.cpp
+++ 구간합.cpp
@@ -18,7 +18,7 @@ public:
 	}
 public:
 	//node 노드가 array[left, right]배열을 표한할 때
-	//node를 루트로 하는 서브트리를 초기화하고, 이 구간의 최소치를 반환한다.
+	//node를 루트로 하는 서브트리를 초기화하고, 이 구간의 합을 반환한다.
 	long long init(const vector<int>& array, int left, int right, int node) {
 		if (left == right)
 			return rangeSum[node] = array[left];
@@ -28,9 +28,9 @@ public:
 
 		return rangeSum[node] = leftSum + rightSum;
 	}
-	//node가 표현하는 범위[nodeLeft, nodeRight]와 우리가 최소치를 찾기 원하는 구간[left, right]의 교집합의 최소원소를 반환한다.
+	//node가 표현하는 범위[nodeLeft, nodeRight]와 우리가 최소치를 찾기 원하는 구간[left, right]의 교집합의 구간 합을 반환한다.
 	long long query(int left, int right, int node, int nodeLeft, int nodeRight) {
-		//두 구간이 겹치지 않으면 아주 큰값을 반환한다: 무시됨
+		//두 구간이 겹치지 않으면 0을 반환한다: 무시됨
 		if (right < nodeLeft || nodeRight < left) 
 			return 0;
 		//node가 표현하는 범위가 array[left, right]에 완전히 포함되는 경우
@@ -40,29 +40,27 @@ public:
 		int mid = (nodeLeft + nodeRight) / 2;
 		return query(left, right, node * 2, nodeLeft, mid) + query(left, right, node * 2 + 1, mid + 1, nodeRight);
 	}
+	//array[index] = newValue로 바뀌었을 때 node를 루트로 하는
+	//구간 트리를 갱신하고 노드가 표현하는 구간의 합을 반환한다.
+	long long update(int index, int newValue, int node, int nodeLeft, int nodeRight) {
+		//index가 노드가 표현하는 구간과 상관없는 경우엔 무시한다.
+		if (index < nodeLeft || nodeRight < index)
+			return rangeSum[node];
+		//트리의 리프까지 내려온 경우
+		if (nodeLeft == nodeRight) return rangeSum[node] = newValue;
+		int mid = (nodeLeft + nodeRight) / 2;
+		return rangeSum[node] = 
+			update(index, newValue, node * 2, nodeLeft, mid) + update(index, newValue, node * 2 + 1, mid + 1, nodeRight);
+	}
 	//query()를 외부에서 호출하기 위한 인터페이스
 	long long query(int left, int right) {
 		return query(left, right, 1, 0, n - 1);
 	}
+	long long update(int index, int newValue) {
+		return update(index, newValue, 1, 0, n - 1);
+	}
 };
 vector<int> arr;
-long long rangeSum[1000000];
-
-void updateRangeSum(int idx) {
-	for (int i = idx; i < N; i++) {
-		if (i == 0) {
-			rangeSum[i] = arr[i];
-			continue;
-		}
-		rangeSum[i] = rangeSum[i - 1] + arr[i];
-	}
-}
-
-long long getRangeSum(int start, int end) {
-	if (start == 0)
-		return rangeSum[end];
-	return rangeSum[end] - rangeSum[start - 1];
-}
 
 int main() {
 	scanf("%d %d %d", &N, &M, &K);
@@ -71,29 +69,17 @@ int main() {
 		scanf("%d", &num);
 		arr.push_back(num);
 	}
-	//RSQ rsq(arr);
-
-	updateRangeSum(0);
+	RSQ rsq(arr);
 
 	int minIdx = 1000000;
-	bool change = false;
 	for (int i = 0; i < M + K; i++) {
 		int a, b, c;
 		scanf("%d %d %d", &a, &b, &c);
 		if (a == 1) {
-			arr[b - 1] = c;
-			minIdx = min(minIdx, b - 1);
-			change = true;
+			rsq.update(b - 1, c);
 		}
-		else if (a == 2) {
-			if (change) {
-				//rsq.init(arr, 0, rsq.n - 1, 1);
-				updateRangeSum(minIdx);
-				minIdx = 1000000;
-				change = false;
-			}
-			printf("%lld\n", getRangeSum(b - 1, c - 1));
-			//printf("%lld\n", rsq.query(b - 1, c - 1));
+		else if (a == 2) {		
+			printf("%lld\n", rsq.query(b - 1, c - 1));
 		}
 	}
 	return 0;
