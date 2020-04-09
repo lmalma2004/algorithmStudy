105e69213a02fe01bdffb304aabd404a687122f7
lmalma2004 <lmalma2004@naver.com>
2020-04-09 17:48:13 +0900

    백준/1937 - 메모리초과

--- 욕심쟁이판다.cpp
+++ 욕심쟁이판다.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <algorithm>
-#include <map>
 #define MAX 500
 using namespace std;
 
@@ -8,39 +7,7 @@ int N;
 int maps[MAX][MAX];
 int moveRow[4] = { -1, 0, 1, 0 };
 int moveCol[4] = { 0, 1, 0 ,-1 };
-unsigned long long visited[40];
-int visitedSize;
-
-class Info {
-public:
-	int row;
-	int col;
-	int preTree;
-	unsigned long long visited[40];
-public:
-	Info(int r, int c, int pT, unsigned long long v[40]) {
-		row = r;
-		col = c;
-		preTree = pT;
-		for (int i = 0; i < visitedSize; i++) {
-			visited[i] = v[i];
-		}
-	}	
-	bool operator<(const Info& info) const {
-		if (row != info.row) 
-			return row < info.row;		
-		if (col != info.col)
-			return col < info.col;
-		if (preTree != info.preTree)
-			return preTree < info.preTree;
-		for (int i = 0; i < visitedSize; i++) {
-			if (visited[i] != info.visited[i])
-				return visited[i] < info.visited[i];
-		}
-		return visited[0] < info.visited[0];
-	}
-};
-map<Info, int> memo;
+int memo[MAX][MAX];
 
 bool isPossible(int row, int col, int preTree) {
 	if (maps[row][col] < preTree)
@@ -56,74 +23,47 @@ bool isRange(int row, int col) {
 	return true;
 }
 
-int getVisitedNum(int row, int col) {
-	return row * N + col;
-}
-int getVisitedIdx(int num) {
-	return num / 64;
-}
-
-bool isVisit(int row, int col) {
-	int visitedNum = getVisitedNum(row, col);
-	int visitedIdx = getVisitedIdx(visitedNum);
-	int visitedLoc = visitedNum % 64;
-	if (visited[visitedIdx] & (1 << visitedLoc))
-		return true;
-	return false;
-}
-
 bool canMove(int row, int col, int tree) {
 	for (int d = 0; d < 4; d++) {
 		int nextRow = row + moveRow[d];
 		int nextCol = col + moveCol[d];
-		if (isRange(nextRow, nextCol) && !isVisit(nextRow, nextCol) && isPossible(nextRow, nextCol, tree))
+		if (isRange(nextRow, nextCol) && isPossible(nextRow, nextCol, tree))
 			return true;
 	}
 	return false;
 }
 
-
-int getMaxDay(int row, int col, int preTree) {
+int getMaxDay(int row, int col) {
 	if (!canMove(row, col, maps[row][col]))
 		return 0;
-
-	Info info(row, col, preTree, visited);
-	auto it = memo.find(info);
-	if (it != memo.end())
-		return (*it).second;
-
-	int visitedNum = getVisitedNum(row, col);
-	int visitedIdx = getVisitedIdx(visitedNum);
-	int visitedLoc = visitedNum % 64;
-	visited[visitedIdx] |= (1 << visitedLoc);
-
+	if (memo[row][col] != -1)
+		return memo[row][col];
+	
 	int maxDay = 1;
-
 	for (int d = 0; d < 4; d++) {
 		int nextRow = row + moveRow[d];
 		int nextCol = col + moveCol[d];	
-		if (isRange(nextRow, nextCol) && !isVisit(nextRow, nextCol) && isPossible(nextRow, nextCol, maps[row][col])) {
-			int day = 1 + getMaxDay(nextRow, nextCol, maps[row][col]);
+		if (isRange(nextRow, nextCol) && isPossible(nextRow, nextCol, maps[row][col])) {
+			if(memo[nextRow][nextCol] == -1)
+				memo[nextRow][nextCol] = getMaxDay(nextRow, nextCol);
+			int day = 1 + memo[nextRow][nextCol];
 			maxDay = max(maxDay, day);
 		}
 	}
 	
-	visited[visitedIdx] &= ~(1 << visitedLoc);
-
-	memo[info] = maxDay;
+	memo[row][col] = maxDay;
 	return maxDay;
 }
 
 int solve() {
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			memo[i][j] = -1;
 	int maxDay = 0;
-	visitedSize = ((N * N) / 64) + 1;
-
-	for (int i = 0; i < visitedSize; i++)
-		visited[i] = 0;
 
 	for(int i=0; i<N; i++)
 		for (int j = 0; j < N; j++) {
-			int day = 1 + getMaxDay(i, j, 0);
+			int day = 1 + getMaxDay(i, j);
 			maxDay = max(maxDay, day);
 		}
 
