83a51462c0b31e10b989c7fe4057edcf0d450d13
lmalma2004 <lmalma2004@naver.com>
2020-04-12 20:15:09 +0900

    구간합으로 시간복잡도 줄임

--- 연습문제/practice_3.cpp
+++ 연습문제/practice_3.cpp
@@ -3,17 +3,14 @@
 
 using namespace std;
 
-//n재료를 가지고 len길이의 도로를 만들 수 있냐
+vector<int> rangeSum;
 
-int getWrongCnt(string& road, int start, int len) {
-    int sum = 0;    
-
-    for (int i = start; i < start + len; i++) {
-        if (road[i] == '0')
-            sum++;
-    }
-    return sum;
+int getWrongCnt(string& road, int start, int len) {       
+    if (start == 0)
+        return rangeSum[len - 1];
+    return rangeSum[start + len - 1] - rangeSum[start - 1];
 }
+//n재료를 가지고 len길이의 도로를 만들 수 있냐
 bool isPossible(string& road, int len, int n) {
     for (int i = 0; i <= road.size() - len; i++) {
         int wrongCnt = getWrongCnt(road, i, len);
@@ -22,10 +19,20 @@ bool isPossible(string& road, int len, int n) {
     }
     return false;
 }
+int makeRangeSum(const string& road, int index) {
+    if (index == 0) {
+        rangeSum[0] = road[0] == '0' ? 1 : 0;
+        return rangeSum[0];
+    }
+    int add = road[index] == '0' ? 1 : 0;
+    return rangeSum[index] = makeRangeSum(road, index - 1) + add;
+}
 
 int solution(string road, int n) {
     int lo = 0;
     int hi = road.length();
+    rangeSum.resize(hi);
+    makeRangeSum(road, road.size() - 1);
 
     while (lo <= hi) {
         int mid = (lo + hi) / 2;
