5307d0446f5308069f1f435b0305f35d84db3e63
lmalma2004 <lmalma2004@naver.com>
2020-04-18 20:47:57 +0900

    백준/10253

--- 5주/헨리.cpp
+++ 5주/헨리.cpp
@@ -1,48 +1,38 @@
 #include <iostream>
 
 using namespace std;
-
 int T;
 
-bool isPossible(int a, int b, int nextB) {
-	int first = a * nextB;
-	int second = b;
-	if (first - second >= 0)
-		return true;
-	return false;
-}
-
-void getNextB(int& a, int& b) {
-	int lo = 1;
-	int hi = b;
-	while (lo <= hi) {	
-		int mid = (lo + hi) / 2;
-		if (isPossible(a, b, mid))
-			hi = mid - 1;
-		else
-			lo = mid + 1;
-	}
-	int first = a * lo;
-	int second = b;
-
-	a = first - second;
-	b *= lo;
+void getNextNumber(int& a, int& b) {
+	int ma, mb;
+	mb = (b / a) + 1;
+	ma = a * mb;
+	a = ma - b;
+	b *= mb;
 }
 
 int solve(int a, int b) {	
 	while (true) {
-		getNextB(a, b);
 		if (a == 1)
 			return b;
+		getNextNumber(a, b);
 	}
 }
 
 int main() {
-	scanf("%d", &T);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> T;
 	for (int tc = 1; tc <= T; tc++) {
 		int a, b;
-		scanf("%d %d", &a, &b);
-		printf("%d\n", solve(a, b));
+		cin >> a;
+		cin >> b;
+		while (true) {
+			if (!(b % a))
+				break;
+			getNextNumber(a, b);
+		}
+		cout << b / a << "\n";
 	}
 	return 0;
 }
