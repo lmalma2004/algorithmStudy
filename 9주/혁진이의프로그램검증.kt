8b343b4548e295e13eb3125f18186cdda60f1818
USER <user@AL01412578.local>
2020-05-12 22:05:41 +0900

    sw expert academy/1824 - kotlin

--- 9주/혁진이의프로그램검증.kt
+++ 9주/혁진이의프로그램검증.kt
@@ -19,20 +19,148 @@ class Program(){
             )}
             )}
             )
+    var memory = 0
+    var direc = RIGHT
+    var row = 0
+    var col = 0
 
-}
-fun main(args: Array<String>){
-    val T = readLine()!!.toInt()
-    for(i in 1..T){
-
-    }
-
-    for(i in maps.indices){
-        val row = readLine()!!.split(" ")
-        for(j in row.indices){
-            maps[i][j] = row[j].toInt()
+    fun move(){
+        when(direc){
+            LEFT-> moveLeft()
+            RIGHT-> moveRight()
+            UP-> moveUp()
+            DOWN-> moveDown()
         }
     }
-    println(solve())
+    fun progress() : Boolean{
+        if(state[row][col][memory][direc])
+            return false
+        state[row][col][memory][direc] = true
+
+        when(arr[row][col]){
+            '<'-> {
+                direc = LEFT
+                move()
+            }
+            '>'->{
+                direc = RIGHT
+                move()
+            }
+            '^'->{
+                direc = UP
+                move()
+            }
+            'v'->{
+                direc = DOWN
+                move()
+            }
+            '_'->{
+                if(memory==0)
+                    direc = RIGHT
+                else
+                    direc = LEFT
+                move()
+            }
+            '|'->{
+                if(memory == 0)
+                    direc = DOWN
+                else
+                    direc = UP
+                move()
+            }
+            '?'->{
+                for(i in 0..3){
+                    direc = i
+                    val memory_orig = memory
+                    val row_orig = row
+                    val col_orig = col
+                    move()
+                    if(progress())
+                        return true
+                    memory = memory_orig
+                    row = row_orig
+                    col = col_orig
+                }
+                return false
+            }
+            '.'->{
+                move()
+            }
+            '@'->{
+                return true
+            }
+            '+'->{
+                if(memory == 15)
+                    memory = 0
+                else
+                    memory++
+                move()
+            }
+            '-'->{
+                if(memory == 0)
+                    memory = 15
+                else
+                    memory--
+                move()
+            }
+            else->{
+                memory = arr[row][col] - '0'
+                move()
+            }
+        }
+        return progress()
+    }
+
+    fun moveLeft(){
+        if(col == 0)
+            col = C - 1
+        else
+            col--
+    }
+    fun moveRight(){
+        if(col == C - 1)
+            col = 0
+        else
+            col++
+    }
+    fun moveUp(){
+        if(row == 0)
+            row = R - 1
+        else
+            row--
+    }
+    fun moveDown(){
+        if(row == R - 1)
+            row = 0
+        else
+            row++
+    }
+}
+
+fun main(args: Array<String>){
+    val T = readLine()!!.toInt()
+    for(tc in 1..T){
+        val row = readLine()!!.split(" ")
+        R = row[0].toInt()
+        C = row[1].toInt()
+        var cnt = 0
+        var pg = Program()
+
+        for(i in 0..R-1){
+            val tmp = readLine()!!
+            for(j in 0..C-1){
+                pg.arr[i][j] = tmp[j]
+                if(tmp[j] == '@')
+                    cnt++
+            }
+        }
+
+        if(cnt == 0)
+            println("#"+tc+" NO")
+        else if(pg.progress())
+            println("#"+tc+" YES")
+        else
+            println("#"+tc+" NO")
+    }
 }
 
